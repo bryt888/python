8eb3919de549c8e17afd62eeaeb4c4e7aea8be54
bryt888 <hardwarediy@gmail.com>
2024-04-28 14:37:48 -0400

    Update facedetect.py

--- opencv/facedetect.py
+++ opencv/facedetect.py
@@ -95,3 +95,58 @@ if face_found:
 
 img = Image.open('face_contours_and_irises.png')
 display(img)
+
+
+##Pose
+# Specify the image filename
+file = 'pose.jpg'
+
+# Create a MediaPipe `Pose` object
+with mp_pose.Pose(static_image_mode=True,
+                  model_complexity=2,
+                  enable_segmentation=True) as pose:
+
+    # Read the file in and get dims
+    image = cv2.imread(file)
+
+    # Convert the BGR image to RGB and then process with the `Pose` object.
+    results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+# Copy the iamge
+annotated_image = image.copy()
+
+# Draw pose, left and right hands, and face landmarks on the image with drawing specification defaults.
+mp_drawing.draw_landmarks(annotated_image,
+                          results.pose_landmarks,
+                          mp_pose.POSE_CONNECTIONS,
+                          landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+
+# Save image with drawing
+filename = "pose_wireframe.png"
+cv2.imwrite(filename, annotated_image)
+
+# Open image
+display(Image.open(filename))
+
+## green Background
+segmented_image = image.copy()
+
+# Value in [0, 1] that says how "tight" to make the segmentation. Greater => tighter
+tightness = .3
+
+# Stacks the segmentation mask for 3 RGB channels, and then creates a filter for which pixels to keep
+condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > tightness
+
+# Creates a black background image
+bg_image = np.zeros(image.shape, dtype=np.uint8)
+
+# Can change the color by specifying (0-255) RGB values below
+bg_image[:] = [4, 244, 4]
+
+# Where the condition is met (i.e. above inclusion threshold), display annotated image
+# Otherwise, use the background color
+segmented_image = np.where(condition, segmented_image, bg_image)
+
+filename = 'pose_green_screen.png'
+cv2.imwrite(filename, segmented_image)
+display(Image.open(filename))
